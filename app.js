8fb693f28053c5f2197071e62a94d009cfec5a3e
Catherine Lau <clau.catherine@gmail.com>
2015-09-19 16:32:41 -0700

    First commit

---
+++ app.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+
+
+
+
+app.listen(3000, function() {
+	console.log("server listening on port 3000");
+});
+
+app.get('/', function(req, res) {
+	res.send("Welcome!");
+});
